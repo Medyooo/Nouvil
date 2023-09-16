6de6caf67b540f4d776a8ce34f0455f42548258d
GITB3-TEST <arbielbaidi6@gmail.com>
2023-09-16 19:32:44 +0200

    ajout on scroll et des conditions javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let nouvil = document.querySelector('.nouvil');
+
+
+
+window.onscroll = ()=>{
+    let value = scrollY;
+    stars.style.left = value + 'px';
+    moon.style.top = value * 3+ 'px';
+    mountains3.style.top = value *  1.7 + 'px';  
+      mountains4.style.top = value *1.2  + 'px';
+    river.style.top = value  + 'px';
+    boat.style.top = value  + 'px';
+    boat.style.left = value * 1.5 + 'px';
+    nouvil.style.fontSize = value  + 'px';
+    if(scrollY >=120){
+    nouvil.style.fontSize = 140  + 'px';
+    nouvil.style.position = 'fixed';
+       if(scrollY >=478){
+        nouvil.style.display = 'none';
+       }else{
+        nouvil.style.display = 'block';
+       }
+       if(scrollY>=235){
+         document.querySelector('.main').style.background = 'linear-gradient(#376281,#10001f)'
+       } else {
+        document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+       }
+    }
+
+}
